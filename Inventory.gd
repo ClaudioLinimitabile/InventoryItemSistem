4e7749f8009e77e02e2b66bfc184f6d80456c115
ClaudioLinimitabile <damicoclaudio71096@gmail.com>
2025-08-20 00:53:06 +0200

    implemented stackable items

--- Inventory.gd
+++ Inventory.gd
@@ -5,18 +5,28 @@ class_name Inventory
 @export var EntityName: String
 @export var Slot: Dictionary[int,Item] = {}
 
-#insert an item WARNING it Overwrite the item inside the slot
+#insert an item. WARNING it Overwrite the item inside the slot
 func InsertItemInSlot(item:Item,slot:int) -> void:
 	Slot[slot]=item
 
-#Swaps the item of two slots
-func SwapItem(item:Item,destination_slot:int) ->void:
+#Swaps the item of two slots if item is the same it ads it to the stack
+#Use FORCE_SWAP true to force the swap of the similar item preventing it to add it to the stack
+func SwapItem(item:Item,destination_slot:int,FORCE_SWAP:bool=false) ->void:
 	var temporal
 	for x in Slot:
 		if Slot[x]==item:
-			temporal = Slot[x]
-			Slot[x] = Slot[destination_slot]
-			Slot[destination_slot] = temporal
+			if Slot[x]==null || Slot[x].Name!=Slot[destination_slot].Name || FORCE_SWAP:
+				temporal = Slot[x]
+				Slot[x] = Slot[destination_slot]
+				Slot[destination_slot] = temporal
+				return
+			if Slot[x].Name==Slot[destination_slot].Name:
+				var qt = Slot[destination_slot].quantity + Slot[x].quantity
+				var NEWI = Slot[destination_slot].duplicate()
+				Slot[destination_slot] = NEWI
+				Slot[destination_slot].quantity = qt
+				Slot[x]=null
+				return
 
 func GetItem(slot:int) -> Item:
 	return Slot[slot]
--- Item.gd
+++ Item.gd
@@ -8,5 +8,13 @@ class_name Item
 
 @export var Stats: Dictionary[String,int] = {}
 
+@export_range(1,99999,1) var Max_Stack: int
+@export_range(1,99999,1) var quantity: int
+
 func GetStatValue(name:String) -> int:
 	return Stats[name]
+
+func IsOverFlow() -> bool:
+	if Max_Stack <= quantity:
+		return true
+	return false
