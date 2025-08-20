26e8b2047b6de2cad211788e8e1e71bdbdeadcd4
ClaudioLinimitabile <damicoclaudio71096@gmail.com>
2025-08-20 08:54:58 +0200

    added splitqt function modified the swap a litle

--- Inventory.gd
+++ Inventory.gd
@@ -10,6 +10,7 @@ func InsertItemInSlot(item:Item,slot:int) -> void:
 	Slot[slot]=item
 
 #Swaps the item of two slots if item is the same it ads it to the stack
+#if item stack overflow the rest of the stack stays to te previous slot
 #Use FORCE_SWAP true to force the swap of the similar item preventing it to add it to the stack
 func SwapItem(item:Item,destination_slot:int,FORCE_SWAP:bool=false) ->void:
 	var temporal
@@ -21,12 +22,32 @@ func SwapItem(item:Item,destination_slot:int,FORCE_SWAP:bool=false) ->void:
 				Slot[destination_slot] = temporal
 				return
 			if Slot[x].Name==Slot[destination_slot].Name:
+				var rs = 0
 				var qt = Slot[destination_slot].quantity + Slot[x].quantity
+				if qt > Slot[x].Max_Stack:
+					rs = qt - Slot[x].Max_Stack
+					qt = Slot[x].Max_Stack
 				var NEWI = Slot[destination_slot].duplicate()
 				Slot[destination_slot] = NEWI
 				Slot[destination_slot].quantity = qt
-				Slot[x]=null
+				if rs < 1:
+					Slot[x]=null
+				else:
+					var NEWX = Slot[x].duplicate()
+					Slot[x] = NEWX
+					Slot[x].quantity = rs
 				return
+
+#create a new item whit quantity desired and returns it if u put over the quantity you get all
+func SplitQuantity(item:Item,qt:int) -> Item:
+	if item.quantity > qt:
+		var newI = item.duplicate()
+		newI.quantity = qt
+		item.quantity -= qt
+		return newI
+	Slot[GetSlot(item)] = null
+	return item
+
 
 func GetItem(slot:int) -> Item:
 	return Slot[slot]
